3d4dd4f73c795580b88a6054439df19a0f3b5282
syedsufiyann <syedsufiyan4567@gmail.com>
2025-10-28 03:24:46 +0530

    Installed Monaco-Editor Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+import { useState } from "react";
+import Editor from "@monaco-editor/react";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const [code, setCode] = useState("// Write your JavaScript here");
+
+  const handleAnalyze = () => {
+    console.log("User code:", code);
+  };
+
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">CodeBuddy Project</h1>
+    <main className="flex flex-col  items-center justify-center bg-gray-800 min-h-screen">
+      <h1 className="text-4xl font-bold mb-6 ">CodeBuddy 🧠</h1>
+      <div className="w-3/4 h-96 mb-4 border border-gray-700 rounded">
+        <Editor
+          height="200px"
+          defaultLanguage="javascript"
+          theme="vs-dark"
+          value={code}
+          onChange={(value) => setCode(value || "")}
+        />
+      </div>
+      <Button onClick={handleAnalyze}>Analyze Code</Button>
     </main>
   );
 }
