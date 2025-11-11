50046e01ab9f5b112faef1048e6d76804198ee8d
syedsufiyann <syedsufiyan4567@gmail.com>
2025-11-11 07:59:48 +0530

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,19 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const [code, setCode] = useState("// Write your JavaScript here");
+  const [ analysis, setAnalysis ] = useState<React.ReactElement | null>(null);
 
   const handleAnalyze = () => {
-    console.log("User code:", code);
+    setAnalysis(
+    <div>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi minima repellendus rerum est sequi soluta consequuntur minus saepe quia, iste sed autem velit quas blanditiis optio earum reprehenderit ab vero error dicta aliquid voluptatibus illo id? Commodi temporibus ratione ea illum consectetur libero, corrupti odit. Necessitatibus sequi repellendus facere nam nemo dignissimos natus aut alias adipisci officiis. Culpa ratione, quidem voluptate est esse adipisci deserunt exercitationem magnam ex eaque, hic deleniti reprehenderit nihil sapiente, voluptates qui aliquam? Doloribus quod omnis quas ut. Repellat aspernatur sunt ullam accusamus. Quibusdam blanditiis maxime odio quae. Quasi laborum reiciendis nobis velit repudiandae ullam fuga?
+    </h2>
+    </div>);
   };
 
   return (
-    <main className="flex flex-col  items-center justify-center bg-gray-800 min-h-screen">
-      <h1 className="text-4xl font-bold mb-6 ">CodeBuddy 🧠</h1>
+    <main className="flex flex-col  items-center justify-center  min-h-screen">
+      <h1 className="text-4xl font-bold mb-6 font">CodeBuddy</h1>
       <div className="w-3/4 h-96 mb-4 border border-gray-700 rounded">
         <Editor
           height="200px"
@@ -23,6 +28,7 @@ export default function Home() {
         />
       </div>
       <Button onClick={handleAnalyze}>Analyze Code</Button>
+      {analysis && <div className="w-3/4 h-96 mb-4 border border-gray-700 rounded bg-gray-700">{analysis}</div>}
     </main>
   );
 }
