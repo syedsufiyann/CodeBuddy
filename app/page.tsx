6166afc141f18fd3e06b7aeee61e85bf6243ad0e
syedsufiyann <syedsufiyan4567@gmail.com>
2025-11-12 01:12:22 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const [code, setCode] = useState("// Write your JavaScript here");
-  const [ analysis, setAnalysis ] = useState<React.ReactElement | null>(null);
+  const [analysis, setAnalysis] = useState<React.ReactElement | null>(null);
 
   const handleAnalyze = () => {
     setAnalysis(
-    <div>
-      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi minima repellendus rerum est sequi soluta consequuntur minus saepe quia, iste sed autem velit quas blanditiis optio earum reprehenderit ab vero error dicta aliquid voluptatibus illo id? Commodi temporibus ratione ea illum consectetur libero, corrupti odit. Necessitatibus sequi repellendus facere nam nemo dignissimos natus aut alias adipisci officiis. Culpa ratione, quidem voluptate est esse adipisci deserunt exercitationem magnam ex eaque, hic deleniti reprehenderit nihil sapiente, voluptates qui aliquam? Doloribus quod omnis quas ut. Repellat aspernatur sunt ullam accusamus. Quibusdam blanditiis maxime odio quae. Quasi laborum reiciendis nobis velit repudiandae ullam fuga?
-    </h2>
-    </div>);
+      <div>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi minima repellendus rerum est sequi soluta consequuntur minus saepe quia, iste sed autem velit quas blanditiis optio earum reprehenderit ab vero error dicta aliquid voluptatibus illo id? Commodi temporibus ratione ea illum consectetur libero, corrupti odit. Necessitatibus sequi repellendus facere nam nemo dignissimos natus aut alias adipisci officiis. Culpa ratione, quidem voluptate est esse adipisci deserunt exercitationem magnam ex eaque, hic deleniti reprehenderit nihil sapiente, voluptates qui aliquam? Doloribus quod omnis quas ut. Repellat aspernatur sunt ullam accusamus. Quibusdam blanditiis maxime odio quae. Quasi laborum reiciendis nobis velit repudiandae ullam fuga?
+        </h2>
+      </div>);
   };
 
   return (
@@ -28,7 +28,8 @@ export default function Home() {
         />
       </div>
       <Button onClick={handleAnalyze}>Analyze Code</Button>
-      {analysis && <div className="w-3/4 h-96 mb-4 border border-gray-700 rounded bg-gray-700">{analysis}</div>}
+      {analysis && <div className="w-3/4 h-96 mb-4 border border-gray-700 rounded bg-gray-700">{analysis}
+      </div>}
     </main>
   );
 }
